3ae7f0f24bc72f415c0345b486aa853c4b194c68
ramses <ramses.santos@gmail.com>
2014-11-07 17:22:03 -0700

    Se agrega la entidad de login

--- app/src/main/java/com/ram/misminutas/Login.java
+++ app/src/main/java/com/ram/misminutas/Login.java
@@ -4,14 +4,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Login extends ActionBarActivity {
+
+    private EditText  username=null;
+    private EditText  password=null;
+    //private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+        //login = (Button)findViewById(R.id.button1);
     }
 
 
@@ -35,5 +48,17 @@ public class Login extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login(View view) {
+        if (username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Redirecting...",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
