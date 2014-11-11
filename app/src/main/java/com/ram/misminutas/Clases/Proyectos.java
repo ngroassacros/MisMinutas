852c5624842b0f644640451fd8bc9a00656c4de9
ramses <ramses.santos@gmail.com>
2014-11-11 11:39:35 -0700

    Se agregan clases para Proyectos y usuarios. Se modifica el android manifest para asignar permisos de internet.

---
+++ app/src/main/java/com/ram/misminutas/Clases/Proyectos.java
@@ -0,0 +1,12 @@
+package com.ram.misminutas.Clases;
+
+/**
+ * Created by ramses on 10/11/14.
+ */
+public class Proyectos
+{
+    public int Id;
+    public String Nombre;
+    public String Descripcion;
+    public int UsuarioCreador;
+}
--- app/src/main/java/com/ram/misminutas/Minutas.java
+++ app/src/main/java/com/ram/misminutas/Minutas.java
@@ -2,16 +2,125 @@ package com.ram.misminutas;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.microsoft.windowsazure.mobileservices.*;
+import com.ram.misminutas.Clases.Usuarios;
+
+import java.net.MalformedURLException;
+import java.util.List;
 
 
 public class Minutas extends ActionBarActivity {
+
+    private MobileServiceClient clienteServicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minutas);
+
+        String nombre = getIntent().getStringExtra("NombreUsuario");
+        this.setTitle(nombre);
+
+        try {
+            clienteServicio = new MobileServiceClient("https://minuta.azure-mobile.net/", "QrKYYqtZLUXlKEafdAgkwFfvOiINOt32", this);
+
+            //final ListView listaMinutas = (ListView) findViewById(R.id.listaMinutas);
+
+            /*Proyectos proy = new Proyectos();
+            proy.Nombre = "ProyectoN";
+            proy.Descripcion = "Descripcion Del Proyecto. " + nombre;
+            proy.UsuarioCreador = 1;
+
+            clienteServicio.getTable(Proyectos.class).insert(proy, new TableOperationCallback<Proyectos>() {
+                public void onCompleted(Proyectos entity, Exception exception, ServiceFilterResponse response) {
+                    if (exception == null) {
+                        //Toast.makeText(Minutas.class, "OK",Toast.LENGTH_LONG);// Insert succeeded
+                        Log.i("OK", exception.getMessage());
+                    } else {
+                        // Insert failed
+                        Log.i("NO OK",exception.getMessage());
+                    }
+                }
+            });*/
+
+            /*Usuarios user = new Usuarios();
+            user.Nombre = nombre;
+            user.Email = "";
+            user.Telefono = 5432;
+
+            clienteServicio.getTable(Usuarios.class).insert(user, new TableOperationCallback<Usuarios>() {
+                public void onCompleted(Usuarios entity, Exception exception, ServiceFilterResponse response) {
+                    if (exception == null) {
+                        //Toast.makeText(Minutas.class, "OK",Toast.LENGTH_LONG);// Insert succeeded
+                        Log.i("OK", exception.getMessage());
+                    } else {
+                        // Insert failed
+                        Log.i("NO OK",exception.getMessage());
+                    }
+                }
+            });*/
+             MobileServiceTable mToDoTable;
+
+
+            mToDoTable = clienteServicio.getTable(Usuarios.class);
+            /*mToDoTable = clienteServicio.getTable("Usuarios");
+
+            mToDoTable.execute(new TableJsonQueryCallback() {
+                @Override
+                public void onCompleted(JsonElement jsonElement, int i, Exception e, ServiceFilterResponse serviceFilterResponse) {
+                    if (e == null) {
+                        Log.i("NO OK",e.getMessage());
+                        jsonElement.getAsString().indexOf(0);
+                    }
+                    else{
+                        Log.i("NO OK",e.getMessage());
+                    }
+                }
+            });*/
+            mToDoTable.execute(new TableQueryCallback<Usuarios>() {
+                @Override
+                public void onCompleted(List<Usuarios> usuarioses, int i, Exception e, ServiceFilterResponse serviceFilterResponse) {
+                    if (e == null) {
+//                        //mAdapter.clear();
+//
+                        for (Usuarios item : usuarioses) {
+                           Log.i("NOMBRE:", item.Nombre);
+
+                        }
+//
+                    } else {
+                        //createAndShowDialog(exception, "Error");
+                        Log.i("NO OK",e.getMessage());
+                        Log.i("CAUSA: ", String.valueOf(e.getCause()));
+                    }
+                }
+            });
+//            mToDoTable.where().field("id").eq(1).execute(new TableQueryCallback<Usuarios>() {
+//
+//                public void onCompleted(List<Usuarios> result, int count, Exception exception, ServiceFilterResponse response) {
+//
+//                    if (exception == null) {
+//                        //mAdapter.clear();
+//
+//                        for (Usuarios item : result) {
+//                            Log.i("NOMBRE:", item.Nombre);
+//                        }
+//
+//                    } else {
+//                        //createAndShowDialog(exception, "Error");
+//                        Log.i("NO OK",exception.getMessage());
+//                    }
+//                }
+//            });
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
 
