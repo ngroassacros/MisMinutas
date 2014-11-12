56e67b5da68078ff0363a281627d82ecfefc2946
ramses <ramses.santos@gmail.com>
2014-11-12 16:18:00 -0700

    Se agrega BD local con las tablas de proyecto y usuarios. Se crea consulta para estas dos tablas, asi como los insert a las mismas. Se puede borrar y actualizar usuarios.

--- app/src/main/java/com/ram/misminutas/Clases/Proyecto.java
+++ app/src/main/java/com/ram/misminutas/Clases/Proyecto.java
@@ -1,5 +1,7 @@
 package com.ram.misminutas.Clases;
 
+import java.util.Date;
+
 /**
  * Created by ramses on 10/11/14.
  */
@@ -8,8 +10,6 @@ public class Proyecto
     public int Id;
     public String Nombre;
     public String Descripcion;
+    public Date FechaCreacion;
     public int UsuarioCreador;
-
-
-    
 }
